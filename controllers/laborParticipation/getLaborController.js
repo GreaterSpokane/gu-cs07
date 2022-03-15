d92668935e30d6381bbac7cc3cfc9a9560c52d11
zfoteff <zfoteff@zagmail.gonzaga.edu>
2022-03-14 17:44:26 -0700

    Fix laborController tests

--- controllers/laborParticipation/getLaborController.js
+++ controllers/laborParticipation/getLaborController.js
@@ -24,23 +24,3 @@ module.exports = async function getLabor(county, year) {
         throw err;
     }
 }
-
-/* Retrieve documents by correlation id */
-module.exports = async function getLabor(corr_id) {
-    try {
-        var data = await LaborParticipationRate.find({ _id: ObjectId(corr_id) });
-        if (data == null) throw new Error('Data not found for given county and year');
-        var result = {
-            'corr_id': data._id,
-            'county': data.county,
-            'state': data.state,
-            'year': data.year,
-            'laborForce': data.laborForce,
-            'laborParticipationRate': data.laborParticipationRate
-        }
-
-        return result
-    } catch (err) {
-        throw err;
-    }
-}
