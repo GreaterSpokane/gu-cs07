3292f46faf4b1aa6c2442126e1307da4587fa7dc
Lucas Abeln <lucasabeln@gmail.com>
2022-03-28 16:10:54 -0700

    Worked on Rest calls

--- DataGathering/REST.py
+++ DataGathering/REST.py
@@ -2,10 +2,19 @@ import requests
 from laborb import *
 from APIcalls import *
 
-def main():
-    cost = housing(2018)
-    response = requests.post('http://0.0.0.0:3000/v1/newMedianHousing', data = {'county': 'Spokane', 'state' : 'WA', 'year' : '2018', 'med_housing_cost' : cost})
+def Housing(year):
+    cost = housing(year)
+    response = requests.post('http://0.0.0.0:3000/v1/newMedianHousing', data = {'county': 'Spokane', 'state' : 'WA', 'med_housing_cost' : cost, 'year' : year})
     return response
+
+def LaborForce(year):
+    force, participation = labor(year)
+    response = requests.post('http://0.0.0.0:3000/v1/newLabor', data = {'county': 'Spokane', 'state' : 'WA', 'year' : year, 'labor_force' : force, 'labor_rate' : participation})
+    return response
+
+
+def main():
+    return Housing(2011)
 
 if __name__ == "__main__":
     code = main()
