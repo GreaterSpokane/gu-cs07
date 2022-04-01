2f2c0071c42ba761cce302dc4261b7dff613dcb3
sawatkins <siawatkins@gmail.com>
2022-04-01 03:40:32 -0700

    consistent naming

--- serializers/housingAffordabilitySerializer.js
+++ serializers/housingAffordabilitySerializer.js
@@ -7,17 +7,17 @@
  module.exports = async function serialize(housingAffordabilityData) {
     var serializedResult = {};
     var serializedYears = [];
-    var serializedHousingAffordability = [];
+    var serializedHousingAffordabilityIndex = [];
 
     for (var i = housingAffordabilityData.data.length - 1; i >= 0; i--) {
         serializedYears.push(housingAffordabilityData.data[i]._id);
-        serializedHousingAffordability.push(housingAffordabilityData.data[i].housingAffordability);
+        serializedHousingAffordabilityIndex.push(housingAffordabilityData.data[i].housingAffordabilityIndex);
     }
 
     serializedResult.county = housingAffordabilityData.county;
-    serializedResult.indicator = "Housing Affordability";
+    serializedResult.indicator = "Housing Affordability Index";
     serializedResult.indicator_id = 'hai';
     serializedResult.years = serializedYears;
-    serializedResult.housingAffordability = serializedHousingAffordability;
+    serializedResult.housingAffordability = serializedHousingAffordabilityIndex;
     return serializedResult;
 }
