37cc4211151a6d63306e3c6bcf9b0066341f43a6
zfoteff <zfoteff@zagmail.gonzaga.edu>
2022-04-07 12:12:15 -0700

    error fix. DONT MERGE

--- controllers/auth/authorizeUserController.js
+++ controllers/auth/authorizeUserController.js
@@ -10,13 +10,12 @@ const { sha512 } = require('./hashController');
 module.exports = async function authorizeUser(username, password) {
     var salt = await getUserSalt(username);
 
-    if (salt.salt == undefined) {
+    if (salt.salt == undefined)
         return {
             'result': 'failure',
             'reason': 'cannot find user in list of registered users',
             'username': username
         };
-    }
 
     var hashPass = sha512(password, salt.salt);
     var data = await AuthUser.findOne({ username: username, password: hashPass.passwordHash }).exec();
--- controllers/auth/createAuthUserController.js
+++ controllers/auth/createAuthUserController.js
@@ -9,25 +9,40 @@ const { saltHashPassword } = require('./hashController');
 /* Retrieve documents using county and year as a composite key  */
 module.exports = async function createAuthUser(username, password) {
 
+    //  Check that the username does not already exist
+    var findUserResult = await AuthUser.findOne({ username: username }).exec();
+    if (findUserResult !== undefined) {
+        var result = {
+            'result': "failed to register user",
+            'reason': "username is already registered",
+            'username': username
+        };
+        return result;
+    }
+
     //  Check that the domain matches allowed email domains
     var addressSignIdx = username.indexOf('@');
     if (addressSignIdx == -1) {
         //  No address sign, cannot sign up user without an email domain
-        return {
+        var result = {
             'result': "failed to register user",
             'reason': "domain does not match allowed domains",
             'username': username
         };
+        return result;
     }
 
     //  TODO: Check that email is a real email
+
     var userDomain = username.substring(addressSignIdx + 1);
-    if (userDomain !== process.env.GSI_DOMAIN && userDomain !== process.env.DEV_DOMAIN)
-        return {
+    if (userDomain !== process.env.GSI_DOMAIN && userDomain !== process.env.DEV_DOMAIN) {
+        var result = {
             'result': "failed to register user",
             'reason': "domain does not match allowed domains",
             'username': username
         };
+        return result;
+    }
 
     var hashPass = saltHashPassword(password);
     var hashedPassword = hashPass.passwordHash;
