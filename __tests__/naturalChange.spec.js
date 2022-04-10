aced15059dd2e8d3d2e6e0b55d1c21f7887adb37
zfoteff <zfoteff@zagmail.gonzaga.edu>
2022-04-10 12:55:03 -0700

    Added get many controller and tests pass

--- __tests__/naturalChange.spec.js
+++ __tests__/naturalChange.spec.js
@@ -81,76 +81,75 @@ describe('Laborforce Participation Rate database model tests', () => {
         const found = await getNaturalChange(DATA.county, DATA.year);
         expect(found.corr_id).toEqual(inserted.corr_id);
     });
+    it('should return true if newly created objects\' data matches the inserted objects\' data', async() => {
+        const inserted = await createNaturalChange(
+            DATA.county,
+            DATA.state,
+            DATA.year,
+            DATA.naturalChange
+        );
+        //  Find new entry in DB with get controller
+        const found = await getNaturalChange(DATA.county, DATA.year);
+        expect(found.corr_id).toEqual(inserted.corr_id);
+        expect(found.county).toEqual(DATA.county);
+        expect(found.state).toEqual(DATA.state);
+        expect(found.year).toEqual(DATA.year);
+    });
+    it('should return true if a mass retrieval call to an empty database returns an empty dataset', async() => {
+        const found_data = await getManyNaturalChange(DATA_COUNTY, '2001', '2003');
+        expect(found_data.data).toEqual([]);
+    });
+
+    it('should return true if a mass retrieval call for a county returns a non-empty data set', async() => {
+        for (var i = 0; i < MASS_DATA.length; i++) {
+            encoded_data = JSON.parse(MASS_DATA[i])
+            await createNaturalChange(
+                encoded_data.county,
+                encoded_data.state,
+                encoded_data.year,
+                encoded_data.naturalChange
+            );
+        }
+
+        const found_data = await getManyNaturalChange(DATA_COUNTY, '2001', '2003');
+        expect(found_data.data).not.toEqual([]);
+    });
+
+    it('should return true if a mass retrieval call for a county year range excludes the proper number of entries', async() => {
+        for (var i = 0; i < MASS_DATA.length; i++) {
+            encoded_data = JSON.parse(MASS_DATA[i])
+            await createNaturalChange(
+                encoded_data.county,
+                encoded_data.state,
+                encoded_data.year,
+                encoded_data.naturalChange
+            );
+        }
+
+        const found_data = await getManyNaturalChange(DATA_COUNTY, '2001', '2002');
+        expect(found_data.data.length).toEqual(2);
+    });
+
+    it('should return true if a mass retrieval for a county that does not exist in the database returns nothing', async() => {
+        const found_data = await getManyNaturalChange("C2", '2001', '2003');
+        expect(found_data.data).toEqual([]);
+    });
+
     /*
-        it('should return true if newly created objects\' data matches the inserted objects\' data', async() => {
-            const inserted = await createNaturalChange(
-                DATA.county,
-                DATA.state,
-                DATA.year,
-                DATA.naturalChange
-            );
-            //  Find new entry in DB with get controller
-            const found = await getNaturalChange(DATA.county, DATA.year);
-            expect(found.county).toEqual(DATA.county);
-            expect(found.state).toEqual(DATA.state);
-            expect(found.year).toEqual(DATA.year);
-        });
-
-        it('should return true if a mass retrieval call to an empty database returns an empty dataset', async() => {
-            const found_data = await getManyNaturalChange(DATA_COUNTY, '2001', '2003');
-            expect(found_data.data).toEqual([]);
-        });
-
-        it('should return true if a mass retrieval call for a county returns a non-empty data set', async() => {
-            for (var i = 0; i < MASS_DATA.length; i++) {
-                encoded_data = JSON.parse(MASS_DATA[i])
-                await createNaturalChange(
-                    encoded_data.county,
-                    encoded_data.state,
-                    encoded_data.year,
-                    encoded_data.naturalData
-                );
-            }
-
-            const found_data = await getManyNaturalChange(DATA_COUNTY, '2001', '2003');
-            expect(found_data.data).not.toEqual([]);
-        });
-
-        it('should return true if a mass retrieval call for a county year range excludes the proper number of entries', async() => {
-            for (var i = 0; i < MASS_DATA.length; i++) {
-                encoded_data = JSON.parse(MASS_DATA[i])
-                await createNaturalChange(
-                    encoded_data.county,
-                    encoded_data.state,
-                    encoded_data.year,
-                    encoded_data.naturalChange
-                );
-            }
-
-            const found_data = await getManyNaturalChange(DATA_COUNTY, '2001', '2002');
-            expect(found_data.data.length).toEqual(2);
-        });
-
-        it('should return true if a mass retrieval for a county that does not exist in the database returns nothing', async() => {
-            const found_data = await getManyNaturalChange("C2", '2001', '2003');
-            expect(found_data.data).toEqual([]);
-        });
-
-        /*
-        it('should successfully delete element from database', async() => {
-            const inserted = await createLabor(
-                DATA.county,
-                DATA.state,
-                DATA.year,
-                Math.random(),
-            );
-            //  Find new entry in DB with get controller
-            const found = await getLabor(DATA.county, DATA.year);
-            expect(found.corr_id).toEqual(inserted.corr_id);
-            const result = await deleteLabor(inserted.corr_id);
-            expect(result.result).toEqual('Success');
-            const does_exist = await getLabor(DATA.county, DATA.year);
-            assert(does_exist.corr_id).toEqual(null);
-        });
-        */
+    it('should successfully delete element from database', async() => {
+        const inserted = await createLabor(
+            DATA.county,
+            DATA.state,
+            DATA.year,
+            Math.random(),
+        );
+        //  Find new entry in DB with get controller
+        const found = await getLabor(DATA.county, DATA.year);
+        expect(found.corr_id).toEqual(inserted.corr_id);
+        const result = await deleteLabor(inserted.corr_id);
+        expect(result.result).toEqual('Success');
+        const does_exist = await getLabor(DATA.county, DATA.year);
+        assert(does_exist.corr_id).toEqual(null);
+    });
+    */
 });
