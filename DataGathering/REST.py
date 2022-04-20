ebe3da3ea6ac56e6722651c383babd6abf341c27
Lucas Abeln <lucasabeln@gmail.com>
2022-04-20 13:29:17 -0700

    More manual forms

--- DataGathering/REST.py
+++ DataGathering/REST.py
@@ -2,23 +2,24 @@ import requests
 from laborb import *
 from APIcalls import *
 
-def Housing(year):
-    cost = housing(year)
-    response = requests.post('http://0.0.0.0:3000/v1/newMedianHousing', data = {'county': 'Spokane', 'state' : 'WA', 'year' : year, 'med_housing_cost' : cost})
+def Housing(countyVal, yearVal, stateVal):
+    cost = housing(countyVal, yearVal)
+    response = requests.post(f'http://0.0.0.0:3000/v1/newMedianHousing?county={countyVal}&state={stateVal}&year={yearVal}&med_housing_cost={cost}')
     return response
 
-def LaborForce(year):
-    force, participation = labor(year)
-    response = requests.post('http://0.0.0.0:3000/v1/newLaborForce', data = {'county': 'Spokane', 'state' : 'WA', 'year' : year, 'labor_force' : force})
+def LaborForce(countyVal, yearVal, stateVal):
+    force, participation = labor(countyVal, yearVal)
+    response = requests.post(f'http://0.0.0.0:3000/v1/newLaborForce?county={countyVal}&state={stateVal}&year={yearVal}&labor_force={force}')
+    response2 = requests.post(f'http://0.0.0.0:3000/v1/newLaborParticipation?county={countyVal}&state={stateVal}&year={yearVal}&labor_participation={participation}')
     return response
 
-def Rent(year):
-    average_rent = housing_month(year)
-    response = requests.post()
+def Rent(countyVal, yearVal, stateVal):
+    average_rent = housing_month(countyVal, year)
+    response = requests.post(f'http://0.0.0.0:3000/v1/newAverageRent?county={countyVal}&state={stateVal}&year={yearVal}&average_rent={average_rent}')
 
 def Income(year):
     median_income = MedianIncome(year)
-    response = requests.post()
+    response = requests.post(f'http://0.0.0.0:3000/v1/newAverageRent?county={countyVal}&state={stateVal}&year={yearVal}&average_rent={average_rent}')
 
 def Employment(countyVal, yearVal, stateVal):
     newEmployment, unemployment = employment(countyVal, yearVal)
@@ -27,7 +28,7 @@ def Employment(countyVal, yearVal, stateVal):
     return response
 
 def main():
-    Employment('Spokane', 2017, 'WA')
+    LaborForce('Boise', 2017, 'ID')
 
 if __name__ == "__main__":
     code = main()
